4ea65ac3a2611de146d5af18d04cdc151886daae
Jean <jeanmatheus0612@gmail.com>
2025-01-23 17:02:04 -0300

    Implementando o CommandLineRunner

--- src/main/java/br/com/alura/ScreenMatch2/ScreenMatch2Application.java
+++ src/main/java/br/com/alura/ScreenMatch2/ScreenMatch2Application.java
@@ -1,13 +1,19 @@
 package br.com.alura.ScreenMatch2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatch2Application {
+public class ScreenMatch2Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeiro projeto Spring sem web");
 	}
 
 }
