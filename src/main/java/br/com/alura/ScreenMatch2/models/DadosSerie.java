e8867419ccd18653b79337d06c75a4def6612487
Jean <jeanmatheus0612@gmail.com>
2025-01-27 22:47:38 -0300

    Desserializando dados

--- src/main/java/br/com/alura/ScreenMatch2/models/DadosSerie.java
+++ src/main/java/br/com/alura/ScreenMatch2/models/DadosSerie.java
@@ -1,5 +1,8 @@
 package br.com.alura.ScreenMatch2.models;
 
-public record DadosSerie() {
-    
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(@JsonAlias("Title") String titulo, 
+                        @JsonAlias("totalSeasons") Integer totalTemporadas, 
+                        @JsonAlias("imdbRating") String avaliacao) {
 }
