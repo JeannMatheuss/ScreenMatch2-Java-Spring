37d341ccc7fefed3a3dfcee2129219122321f8bc
Jean <jeanmatheus0612@gmail.com>
2025-01-31 20:39:17 -0300

    Adicionando os atributos e usando o JsonAlias para especificar o que deve ser pego do Json para uma var Java

--- src/main/java/br/com/alura/ScreenMatch2/models/DadosEpisodio.java
+++ src/main/java/br/com/alura/ScreenMatch2/models/DadosEpisodio.java
@@ -1,4 +1,9 @@
 package br.com.alura.ScreenMatch2.models;
 
-record DadosEpisodio() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+record DadosEpisodio(@JsonAlias("Title") String titulo, 
+                    @JsonAlias("Episode") Integer numero,
+                    @JsonAlias("imdbRating") String avaliacao,
+                    @JsonAlias("Released") String dataLancamento) {
 } 
