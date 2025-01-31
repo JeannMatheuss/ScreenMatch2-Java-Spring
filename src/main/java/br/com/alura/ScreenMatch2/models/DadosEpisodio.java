3db730291df44962793602343b6a604fba50c2c8
Jean <jeanmatheus0612@gmail.com>
2025-01-31 20:42:20 -0300

    Aplicando o JsonIgnoreProperties para ignorar qualquer dado Json que não seja os atributos da classe DadosEpisodios

--- src/main/java/br/com/alura/ScreenMatch2/models/DadosEpisodio.java
+++ src/main/java/br/com/alura/ScreenMatch2/models/DadosEpisodio.java
@@ -1,7 +1,9 @@
 package br.com.alura.ScreenMatch2.models;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 record DadosEpisodio(@JsonAlias("Title") String titulo, 
                     @JsonAlias("Episode") Integer numero,
                     @JsonAlias("imdbRating") String avaliacao,
