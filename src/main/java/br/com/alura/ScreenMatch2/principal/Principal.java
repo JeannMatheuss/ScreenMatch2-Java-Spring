b757af6847344ebf2d38d4dbcc497efb467cb8ce
Jean <jeanmatheus0612@gmail.com>
2025-02-05 20:27:42 -0300

    Criando o meuno de interação com o usuário na classe principal

--- src/main/java/br/com/alura/ScreenMatch2/principal/Principal.java
+++ src/main/java/br/com/alura/ScreenMatch2/principal/Principal.java
@@ -1,5 +1,21 @@
 package br.com.alura.ScreenMatch2.principal;
 
+import java.util.Scanner;
+
+import br.com.alura.ScreenMatch2.service.ConsumoApi;
+
 public class Principal {
-    
+    public void exibeMenu() {
+        Scanner leitura = new Scanner(System.in);
+        ConsumoApi consumo = new ConsumoApi();
+
+        final String ENDERECO = "https://www.omdbapi.com/?t=";
+        final String API_KEY = "&apikey=3493ff3d";
+
+        System.out.println("Digite o nome da série para a busca: ");
+        var nomeSerie = leitura.nextLine();
+        var consumoApi = new ConsumoApi();
+		var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        
+    }
 }
