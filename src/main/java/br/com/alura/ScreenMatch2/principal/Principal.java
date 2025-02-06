d315b268cbe37683a373a3c1d70f04a70afc8932
Jean <jeanmatheus0612@gmail.com>
2025-02-06 09:24:34 -0300

    Implementando dados das temporadas

--- src/main/java/br/com/alura/ScreenMatch2/principal/Principal.java
+++ src/main/java/br/com/alura/ScreenMatch2/principal/Principal.java
@@ -1,8 +1,11 @@
 package br.com.alura.ScreenMatch2.principal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import br.com.alura.ScreenMatch2.models.DadosSerie;
+import br.com.alura.ScreenMatch2.models.DadosTemporada;
 import br.com.alura.ScreenMatch2.service.ConsumoApi;
 import br.com.alura.ScreenMatch2.service.ConverterDados;
 
@@ -21,5 +24,17 @@ public class Principal {
 		var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		if (dados.totalTemporadas() != null) {
+			for (int i = 1; i <= dados.totalTemporadas(); i++) {
+				json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+				DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+				temporadas.add(dadosTemporada);
+			}
+		}		
+
+        temporadas.forEach(System.out::println);
     }
 }
